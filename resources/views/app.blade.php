2ffb470261bdfeab658b43f58b169a48f8c83e59
Christ Akadje <christakadje@gmail.com>
2026-04-08 19:20:40 +0200

    Initial commit - Gestion Projet Ya Consulting

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr" class="light">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title inertia>Ya Consulting</title>
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
+    @inertiaHead
+</head>
+<body class="bg-gray-50 text-gray-900 antialiased">
+    @inertia
+</body>
+</html>

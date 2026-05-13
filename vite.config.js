2eb19e643217baeb7765a007318603a2be31c96e
nathanakadje <christakadje@gmail.com>
2026-05-13 14:43:00 +0200

    Fix build path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+    base: process.env.NODE_ENV === "production" ? "/gestion/build/" : "/",
     plugins: [
         laravel({
             // input: "resources/js/app.js",

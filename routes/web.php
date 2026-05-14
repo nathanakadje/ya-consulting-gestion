794b276aa736cffdf6dcd3d481532df4b0829da9
nathanakadje <christakadje@gmail.com>
2026-05-15 00:05:30 +0200

    Final build with correct asset paths for /gestion

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
-])->group(function () {
+])->prefix('gestion')->group(function () {
     // Route::get('/dashboard', function () {
     //     return Inertia::render('Dashboard/Index');
     // })->name('dashboard');
